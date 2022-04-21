ae3e656f4be85c0caab8be9c8047e1d339415cba
wbtate <tatew@ucsc.edu>
2022-04-20 22:32:29 -0700

    Added temp menu, starts with space

---
+++ src/scenes/Menu.js
@@ -0,0 +1,38 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+    preload() {
+    }
+
+    create(){
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+              top: 5,
+              bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Mining Mole', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000';
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press Space to Begin', menuConfig).setOrigin(0.5);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    }
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+          game.settings = {
+          }
+          //play start sfx
+          this.scene.start('playScene');
+        }
+    }
+}
