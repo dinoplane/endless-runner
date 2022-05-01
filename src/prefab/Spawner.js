bb0eb86de52ce6c75d106257710d77262f03a172
dinoplane <isacsac2017@gmail.com>
2022-04-30 22:41:03 -0700

    Revert "Revert "qol updates""
    This reverts commit 5591e116bccd767b73faccbc82f4066765d5f3da.

--- src/prefab/Spawner.js
+++ src/prefab/Spawner.js
@@ -77,12 +77,14 @@ class Spawner{
         obstacle.plane = plane;
         if (obstacle.plane == 0){
             obstacle.scale = 1;
-            obstacle.y = this.POSITIONS[0].y;
+            obstacle.y = this.POSITIONS[0].y+20;
+            obstacle.setVelocityX(this.mole.speed*-100);
         } else {
             obstacle.scale = this.SCALE;
-            obstacle.y = this.POSITIONS[1].y;
+            obstacle.y = this.POSITIONS[1].y+10;
+            obstacle.setVelocityX(this.mole.speed*-70);
         }
-        obstacle.setVelocityX(this.mole.speed*-100);
+        
         this.nextObstacleDistance = Phaser.Math.Between(this.spawnMin, this.spawnMax);
         return obstacle;
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -117,6 +117,9 @@ class Play extends Phaser.Scene {
            if (this.gameOver) {
                this.goneFar=false;
                 this.ended=false;
+                this.gemSpawner.updateOdds();
+                distance=0;
+                highScore=0;
                 this.scene.restart();
            }
         }); 
@@ -233,7 +236,7 @@ class Play extends Phaser.Scene {
 
             highScore+=distance;
             this.scoreLeft.text=highScore;
-            this.scoreRight.text=distance;
+            //this.scoreRight.text=distance;
             
             if(!this.goneFar&&distance>=2000){
                 this.gemSpawner.updateOdds(1,3,5);
